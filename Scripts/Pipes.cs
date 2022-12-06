5ae70ebb0e4f2ecf3a3e3c54208b6efa881db2ac
JoeyRFS <51177843+joeyrfs@users.noreply.github.com>
2022-12-06 17:55:17 -0300

    Ajustado

--- Scripts/Pipes.cs
+++ Scripts/Pipes.cs
@@ -16,5 +16,13 @@ public class Pipes : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
+        Debug.Log(transform.position.x);
+        if (transform.position.x < -4) {
+            Destroy(gameObject);
+        }
+
+        
+
+
     }
 }
